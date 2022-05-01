081d1256bb89e2ad819311defc57207cf2f0288f
Jiufen <daniel.pardo@timba.co>
2022-04-30 23:02:02 -0500

    Change theme to dark and add animation to tittle screen

--- Assets/Scripts/GamesModules/SceneManagerModule/Scripts/Home/HomeSceneController.cs
+++ Assets/Scripts/GamesModules/SceneManagerModule/Scripts/Home/HomeSceneController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace JiufenGames.MineSweeperAlike.SceneManagement
 {
@@ -12,22 +13,24 @@ namespace JiufenGames.MineSweeperAlike.SceneManagement
     {
         #region ----Fields----
         public TMP_Text titleLabel;
+        public Image playButtonCover;
         #endregion ----Fields----
 
         #region ----Methods----
         public override void Init<T>(T _data, Action<bool> _callback = null)
         {
             _callback?.Invoke(true);
-        }
-
-        [ContextMenu("Press me")]
-        public void ShowTitle()
-        {
             StartCoroutine(TextAppearingAnimation());
         }
 
         IEnumerator TextAppearingAnimation()
         {
+            titleLabel.alpha = 0;
+
+            var tempColor = playButtonCover.color;
+            tempColor.a = 1f;
+            playButtonCover.color = tempColor;
+
             titleLabel.ForceMeshUpdate();
             TMP_TextInfo textInfo = titleLabel.textInfo;
 
@@ -36,27 +39,40 @@ namespace JiufenGames.MineSweeperAlike.SceneManagement
             for (int i = 0; i < characterCount; i++)
             {
                 int timer = 0;
-                while (timer < 100)
+                byte alpha = 0;
+                while (timer < 50)
                 {
                     TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
                     if (!charInfo.isVisible)
                         continue;
-                    int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
 
+                    int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+                    Color32[] newVertexColors = textInfo.meshInfo[materialIndex].colors32;
                     int vertexIndex = textInfo.characterInfo[i].vertexIndex;
 
                     Vector3[] destinationVertices = textInfo.meshInfo[materialIndex].vertices;
                     for (int k = 0; k < 4; k++)
-                        destinationVertices[vertexIndex + k] += new Vector3(0, 1.5f);
+                    {
+                        destinationVertices[vertexIndex + k] += new Vector3(0, 3f);
+                        newVertexColors[vertexIndex + k].a = alpha;
+                    }
 
                     textInfo.meshInfo[materialIndex].mesh.vertices = textInfo.meshInfo[materialIndex].vertices;
                     titleLabel.UpdateGeometry(textInfo.meshInfo[materialIndex].mesh, materialIndex);
+                    titleLabel.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+                    Debug.Log(alpha);
+                    alpha += 5;
                     timer++;
                     yield return null;
                 }
                 yield return new WaitForSeconds(0.05f);
             }
-            //TODO: Color
+            LeanTween.value(1, 0, 2).setOnUpdate((float value) =>
+             {
+                 tempColor = playButtonCover.color;
+                 tempColor.a = value;
+                 playButtonCover.color = tempColor;
+             });
         }
 
         public void GoToGameplay()
