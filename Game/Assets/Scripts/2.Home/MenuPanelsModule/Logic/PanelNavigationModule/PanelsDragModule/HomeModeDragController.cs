d641f422704ee4e00806e61bdb2f9e492a8d226f
Jiufen <pardod312@gmail.com>
2022-06-20 19:02:19 -0500

    Fix

--- Game/Assets/Scripts/2.Home/MenuPanelsModule/Logic/PanelNavigationModule/PanelsDragModule/HomeModeDragController.cs
+++ Game/Assets/Scripts/2.Home/MenuPanelsModule/Logic/PanelNavigationModule/PanelsDragModule/HomeModeDragController.cs
@@ -1,10 +1,42 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-	public class HomeModeDragController : MonoBehaviour
-	{
-		#region ----Fields----
-		#endregion ----Fields----
-		
-		#region ----Methods----	
-		#endregion ----Methods----	
-	}
+public class HomeModeDragController : MonoBehaviour
+{
+    #region ----Fields----
+    #endregion ----Fields----
+
+    #region ----Methods----	
+    public ScrollRect scrollRect;
+    public RectTransform contentPanel;
+    public List<RectTransform> panelsRectTransforms;
+
+    private bool isChecking = false;
+    private int currentPanel = 0;
+    public void CheckvalueOfScroll(Vector2 scroll)
+    {
+        if (isChecking)
+            return;
+        if (scroll.x == 0)
+            return;
+
+        isChecking = true;
+        if (scroll.x < 0 && currentPanel > 0)
+            currentPanel--;
+        else if (scroll.x > 0 && currentPanel + 1 < panelsRectTransforms.Count)
+            currentPanel++;
+
+        SnapTo(panelsRectTransforms[currentPanel]);
+
+    }
+    public void SnapTo(RectTransform target)
+    {
+        Canvas.ForceUpdateCanvases();
+
+        contentPanel.anchoredPosition =
+                (Vector2)scrollRect.transform.InverseTransformPoint(contentPanel.position)
+                - (Vector2)scrollRect.transform.InverseTransformPoint(target.position);
+    }
+    #endregion ----Methods----	
+}
